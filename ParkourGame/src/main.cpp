f036a26d7b25c0e4ecb6305cc53722eadae9b667
ancues02 <ancues02@ucm.es>
2021-05-11 14:15:07 +0200

    Creada estructura inicial del proyecto de parkour

---
+++ ParkourGame/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <PinaMotor.h>
+int main()
+{
+    std::cout << "Hello World!\n";
+    PinaMotor motor;
+    motor.init("Test");
+
+    motor.launch("escena");
+
+    motor.close();
+}
