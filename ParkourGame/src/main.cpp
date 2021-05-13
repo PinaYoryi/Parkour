0b990e76bd5b198366da33c225a32711ef1efe0d
ancues02 <ancues02@ucm.es>
2021-05-13 16:41:09 +0200

    añadido diferencia en main entre debug y release

--- ParkourGame/src/main.cpp
+++ ParkourGame/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <PinaMotor.h>
-int main()
-{
+
+#if (defined _DEBUG)
+#include <crtdbg.h>
+int main() {
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
+#else
+#include <windows.h>
+int WINAPI
+WinMain(HINSTANCE zhInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
     std::cout << "Hello World!\n";
     PinaMotor motor;
     motor.init("Test");
