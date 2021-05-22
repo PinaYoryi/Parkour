af49c8bde403fe078c0537469bc0237ba8647eb6
Daniel González Cerdeiras <daniel20@ucm.es>
2021-05-22 17:52:54 +0200

    Arreglado bat de resources

--- ParkourGame/assets/resources.bat
+++ ParkourGame/assets/resources.bat
@@ -1,6 +1,6 @@
 cd ../../P32021/bin
 copy /Y resources.cfg "../../ParkourGame/bin/resources.cfg"
-cd ../../ParkourGame/assets
+cd ../ParkourGame/assets
 echo [General] >> ../bin/resources.cfg
-echo FileSystem=../assets/OgreResources >> ../bin/resources.cfg
-for %%f in (*.zip) do echo Zip=../assets/OgreResources/%%~nxf >> ../bin/resources.cfg
+echo FileSystem=%CD%/resources >> ../bin/resources.cfg
+for /r %CD% %%f in (*.zip) do echo Zip=%CD%/resources/%%~nxf >> ../bin/resources.cfg
