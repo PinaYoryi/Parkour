46c9063552e1a3b9553c956ef7c3e13f76c43258
ancues02 <ancues02@ucm.es>
2021-05-27 14:47:41 +0200

    vuelto a poner las dependencies

--- ParkourGame/ParkourDependencies.bat
+++ ParkourGame/ParkourDependencies.bat
@@ -10,12 +10,12 @@ if not exist ../../ParkourGame/bin/fmodL64.dll goto copydependencies
 if not exist ../../ParkourGame/bin/lua54.dll goto copydependencies
 if not exist ../../ParkourGame/bin/OgreMain.dll goto copydependencies
 if not exist ../../ParkourGame/bin/OgreMain_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgreMeshLodGenerator.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgreMeshLodGenerator_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgrePaging.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgrePaging_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgreProperty.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/OgreProperty_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgreMeshLodGenerator.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgreMeshLodGenerator_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgrePaging.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgrePaging_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgreProperty.dll goto copydependencies
+if not exist ../../ParkourGame/bin/OgreProperty_d.dll goto copydependencies
 if not exist ../../ParkourGame/bin/OgreRTShaderSystem.dll goto copydependencies
 if not exist ../../ParkourGame/bin/OgreRTShaderSystem_d.dll goto copydependencies
 if not exist ../../ParkourGame/bin/Plugin_ParticleFX.dll copydependencies
@@ -35,18 +35,18 @@ if not exist ../../ParkourGame/bin/CEGUICoreWindowRendererSet.dll goto copydepen
 if not exist ../../ParkourGame/bin/CEGUICoreWindowRendererSet_d.dll goto copydependencies
 if not exist ../../ParkourGame/bin/freetype.dll goto copydependencies
 if not exist ../../ParkourGame/bin/freetype_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/glew.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/glew_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/glfw.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/glfw_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/jpeg.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/jpeg_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/glew.dll goto copydependencies
+if not exist ../../ParkourGame/bin/glew_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/glfw.dll goto copydependencies
+if not exist ../../ParkourGame/bin/glfw_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/jpeg.dll goto copydependencies
+if not exist ../../ParkourGame/bin/jpeg_d.dll goto copydependencies
 if not exist ../../ParkourGame/bin/libexpat.dll goto copydependencies
 if not exist ../../ParkourGame/bin/libexpat_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/libpng.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/libpng_d.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/pcre.dll goto copydependencies
-REM if not exist ../../ParkourGame/bin/pcre_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/libpng.dll goto copydependencies
+if not exist ../../ParkourGame/bin/libpng_d.dll goto copydependencies
+if not exist ../../ParkourGame/bin/pcre.dll goto copydependencies
+if not exist ../../ParkourGame/bin/pcre_d.dll goto copydependencies
 if not exist ../../ParkourGame/bin/SILLY.dll goto copydependencies
 if not exist ../../ParkourGame/bin/SILLY_d.dll goto copydependencies
 
@@ -63,12 +63,12 @@ copy /Y fmodL64.dll "../../ParkourGame/bin/fmodL64.dll"
 copy /Y lua54.dll "../../ParkourGame/bin/lua54.dll"
 copy /Y OgreMain.dll "../../ParkourGame/bin/OgreMain.dll"
 copy /Y OgreMain_d.dll "../../ParkourGame/bin/OgreMain_d.dll"
-REM copy /Y OgreMeshLodGenerator.dll "../../ParkourGame/bin/OgreMeshLodGenerator.dll"
-REM copy /Y OgreMeshLodGenerator_d.dll "../../ParkourGame/bin/OgreMeshLodGenerator_d.dll"
-REM copy /Y OgrePaging.dll "../../ParkourGame/bin/OgrePaging.dll"
-REM copy /Y OgrePaging_d.dll "../../ParkourGame/bin/OgrePaging_d.dll"
-REM copy /Y OgreProperty.dll "../../ParkourGame/bin/OgreProperty.dll"
-REM copy /Y OgreProperty_d.dll "../../ParkourGame/bin/OgreProperty_d.dll"
+copy /Y OgreMeshLodGenerator.dll "../../ParkourGame/bin/OgreMeshLodGenerator.dll"
+copy /Y OgreMeshLodGenerator_d.dll "../../ParkourGame/bin/OgreMeshLodGenerator_d.dll"
+copy /Y OgrePaging.dll "../../ParkourGame/bin/OgrePaging.dll"
+Copy /Y OgrePaging_d.dll "../../ParkourGame/bin/OgrePaging_d.dll"
+copy /Y OgreProperty.dll "../../ParkourGame/bin/OgreProperty.dll"
+copy /Y OgreProperty_d.dll "../../ParkourGame/bin/OgreProperty_d.dll"
 copy /Y OgreRTShaderSystem.dll "../../ParkourGame/bin/OgreRTShaderSystem.dll"
 copy /Y OgreRTShaderSystem_d.dll "../../ParkourGame/bin/OgreRTShaderSystem_d.dll"
 copy /Y Plugin_ParticleFX.dll "../../ParkourGame/bin/Plugin_ParticleFX.dll"
@@ -88,18 +88,18 @@ copy /Y CEGUICoreWindowRendererSet.dll "../../ParkourGame/bin/CEGUICoreWindowRen
 copy /Y CEGUICoreWindowRendererSet_d.dll "../../ParkourGame/bin/CEGUICoreWindowRendererSet_d.dll"
 copy /Y freetype.dll "../../ParkourGame/bin/freetype.dll"
 copy /Y freetype_d.dll "../../ParkourGame/bin/freetype_d.dll"
-REM copy /Y glew.dll "../../ParkourGame/bin/glew.dll"
-REM copy /Y glew_d.dll "../../ParkourGame/bin/glew_d.dll"
-REM copy /Y glfw.dll "../../ParkourGame/bin/glfw.dll"
-REM copy /Y glfw_d.dll "../../ParkourGame/bin/glfw_d.dll"
-REM copy /Y jpeg.dll "../../ParkourGame/bin/jpeg.dll"
-REM copy /Y jpeg_d.dll "../../ParkourGame/bin/jpeg_d.dll"
+copy /Y glew.dll "../../ParkourGame/bin/glew.dll"
+copy /Y glew_d.dll "../../ParkourGame/bin/glew_d.dll"
+copy /Y glfw.dll "../../ParkourGame/bin/glfw.dll"
+copy /Y glfw_d.dll "../../ParkourGame/bin/glfw_d.dll"
+copy /Y jpeg.dll "../../ParkourGame/bin/jpeg.dll"
+copy /Y jpeg_d.dll "../../ParkourGame/bin/jpeg_d.dll"
 copy /Y libexpat.dll "../../ParkourGame/bin/libexpat.dll"
 copy /Y libexpat_d.dll "../../ParkourGame/bin/libexpat_d.dll"
-REM copy /Y libpng.dll "../../ParkourGame/bin/libpng.dll"
-REM copy /Y libpng_d.dll "../../ParkourGame/bin/libpng_d.dll"
-REM copy /Y pcre.dll "../../ParkourGame/bin/pcre.dll"
-REM copy /Y pcre_d.dll "../../ParkourGame/bin/pcre_d.dll"
+copy /Y libpng.dll "../../ParkourGame/bin/libpng.dll"
+copy /Y libpng_d.dll "../../ParkourGame/bin/libpng_d.dll"
+copy /Y pcre.dll "../../ParkourGame/bin/pcre.dll"
+copy /Y pcre_d.dll "../../ParkourGame/bin/pcre_d.dll"
 copy /Y SILLY.dll "../../ParkourGame/bin/SILLY.dll"
 copy /Y SILLY_d.dll "../../ParkourGame/bin/SILLY_d.dll"
 
