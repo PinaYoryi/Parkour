e80a6a27c3def7ed999048550c83765b3784875c
ancues02 <ancues02@ucm.es>
2021-05-27 17:15:42 +0200

    set up para menu controles
    falta cambiar la imagen de menucontroles.lua y la imagen del boton del menuprincipal.lua a uno que ponga controles

--- ParkourGame/assets/resources/scene/menuprincipal.lua
+++ ParkourGame/assets/resources/scene/menuprincipal.lua
@@ -36,8 +36,8 @@ local mapa = {
                 },
                 button={
                     text="",
-                    position="0.3,0.45",
-                    size="240,100",
+                    position="0.3,0.40",
+                    size="240,70",
                     name="botonJugar",
                     properties="NormalImage,TaharezLook/ButtonJugar,HoverImage,TaharezLook/ButtonJugarHover,PushedImage,TaharezLook/ButtonJugarPushed",
                     type="CHANGE_SCENE",
@@ -49,7 +49,7 @@ local mapa = {
      	    }
         },
         {
-            name="BotonS",
+            name="BotonO",
             id=3,
             components={
                 transform={
@@ -60,8 +60,32 @@ local mapa = {
                 },
                 button={
                     text="",
-                    position="0.3,0.7",
-                    size="240,100",
+                    position="0.3,0.57",
+                    size="240,70",
+                    name="botonOpciones",
+                    properties="NormalImage,TaharezLook/ButtonJugar,HoverImage,TaharezLook/ButtonJugarHover,PushedImage,TaharezLook/ButtonJugarPushed",
+                    type="CHANGE_SCENE",
+                    showCursor="true",
+                    cursorImage="TaharezLook/MouseGeorgi",
+                    continueBullet="true",
+                    nextScene="menucontroles.lua"
+                }
+     	    }
+        },
+        {
+            name="BotonS",
+            id=4,
+            components={
+                transform={
+                    position="0,0,0",
+                    rotation="0,0,0",
+                    scale="1,1,1",
+                    parent="-1"
+                },
+                button={
+                    text="",
+                    position="0.3,0.75",
+                    size="240,70",
                     name="botonSalir",
                     properties="NormalImage,TaharezLook/ButtonSalir,HoverImage,TaharezLook/ButtonSalirHover,PushedImage,TaharezLook/ButtonSalirPushed",
                     type="EXIT",
